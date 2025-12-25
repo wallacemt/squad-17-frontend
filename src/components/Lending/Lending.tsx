9f82cd1074161bf723568d8a4d1741e69ad28765
Wallace Santana <wallacesantanak0@gmail.com>
2025-12-24 22:17:29 -0300

    fix(lending): fix erro for folder

--- src/components/Lending/Lending.tsx
+++ src/components/Lending/Lending.tsx
@@ -1,10 +1,11 @@
 import type { TMDBMedia } from "@/types/tmdb";
-import CTA from "./_components/cta";
+
 import Footer from "./_components/footer";
 import Navbar from "./_components/navbar";
 import Features from "./_components/features";
 import Hero from "./_components/hero";
 import MovieCarousel from "./_components/movie-carousel";
+import CTA from "./_components/cta";
 
 export default function LendingPage({ trending }: { trending: TMDBMedia[] }) {
   return (
